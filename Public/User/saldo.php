3a25069c194b69dfd02bea30b66917eba5ec8e39
Laras Maharani <larasmharani@gmail.com>
2023-01-09 20:13:13 +0700

    Add banner ket. dana pengsiun

--- Public/User/saldo.php
+++ Public/User/saldo.php
@@ -128,31 +128,23 @@
             </nav>
         </header>
 
-        <div class="w-full flex flex-col h-screen overflow-y-hidden">
-            <!-- Desktop Header -->
-            <header class="w-full items-center bg-white py-2 px-6 hidden sm:flex">
-                <div class="w-1/2"></div>
-                <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
-                    <button @click="isOpen = !isOpen" class="realtive z-10 w-12 h-12 rounded-full overflow-hidden border-4 border-gray-400 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
-                        <img src="../../dist/images/Profile.png">
-                    </button>
-                    <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
-                    <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
-                        <a href="#" class="block px-4 py-2 account-link hover:text-white">Account</a>
-                    </div>
-                </div>
-            </header>
+        <div class="w-full h-screen overflow-x-hidden border-t flex flex-col">
+            <main class="w-full flex-grow p-6">
+                <div class="flex flex-wrap">
+                    <form class="p-10 bg-white rounded shadow-xl container">
 
-            <!-- Mobile Header & Nav -->
-            <header x-data="{ isOpen: false }" class="bg-[#152A38] w-full bg-sidebar py-5 px-6 sm:hidden">
-                <div class="flex items-center justify-between">
-                    <a href="index.php" class="text-white text-3xl font-semibold uppercase hover:text-gray-300">User</a>
-                    <button @click="isOpen = !isOpen" class="text-white text-3xl focus:outline-none">
-                        <i x-show="!isOpen" class="fas fa-bars"></i>
-                        <i x-show="isOpen" class="fas fa-times"></i>
-                    </button>
+                        <div class="container">
+                                <div class="row">
+                                    <div class="col-sm-2">
+                                        Nama <br />
+                                        Golongan <br />
+                                        Dana Pengsiun <br /><br>
+                                    </div>
+                                </div>
+                        </div>
+                    </form>
                 </div>
-        </div>
+        </div><br>
     </div>
     </main><br>
 

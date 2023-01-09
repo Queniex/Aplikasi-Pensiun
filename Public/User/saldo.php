7ae3494f33d63810e26d5ebec1470b6af9861e99
Laras Maharani <larasmharani@gmail.com>
2023-01-09 20:05:29 +0700

    Add Header

--- Public/User/saldo.php
+++ Public/User/saldo.php
@@ -128,12 +128,28 @@
             </nav>
         </header>
 
-        </main>
+        <div class="w-full flex flex-col h-screen overflow-y-hidden">
+            <!-- Desktop Header -->
+            <header class="w-full items-center bg-white py-2 px-6 hidden sm:flex">
+                <div class="w-1/2"></div>
+                <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
+                    <button @click="isOpen = !isOpen" class="realtive z-10 w-12 h-12 rounded-full overflow-hidden border-4 border-gray-400 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
+                        <img src="../../dist/images/Profile.png">
+                    </button>
+                    <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
+                    <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
+                        <a href="#" class="block px-4 py-2 account-link hover:text-white">Account</a>
+                    </div>
+                </div>
+            </header>
+        </div>
+    </div>
+    </main><br>
 
-        <footer class="w-full bg-white text-center p-4">
-            Copyright to <a target="_blank" href="https://github.com/Queniex/Aplikasi-Pensiun" class="underline text-[#152A38] hover:text-blue-500">Kelompok 3</a><br>
-            All Right Reserved
-        </footer>
+    <footer class="w-full bg-white text-center p-4">
+        Copyright to <a target="_blank" href="https://github.com/Queniex/Aplikasi-Pensiun" class="underline text-[#152A38] hover:text-blue-500">Kelompok 3</a><br>
+        All Right Reserved
+    </footer>
     </div>
 
     </div>

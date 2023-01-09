a70be5053edde24024a3fbc63c64d808017f1709
Laras Maharani <larasmharani@gmail.com>
2023-01-09 19:49:32 +0700

    Saldo Desain

--- Public/User/saldo.php
+++ Public/User/saldo.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,24 +13,22 @@
 
     <script>
         tailwind.config = {
-          theme: {
-            container: {
-              center: true,
-              padding: '16px'
-            },
-            extend: {
-              colors: {
-              },
-              screens: {
-                '2xl': '1320px',
-              },
-              keyframes:{
-              }
+            theme: {
+                container: {
+                    center: true,
+                    padding: '16px'
+                },
+                extend: {
+                    colors: {},
+                    screens: {
+                        '2xl': '1320px',
+                    },
+                    keyframes: {}
+                }
             }
-          }
         }
-      </script>
-    
+    </script>
+
     <style type="text/tailwindcss">
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
         .font-family-inter{ font-family: 'Inter', sans-serif; }
@@ -45,6 +44,7 @@
         } */
     </style>
 </head>
+
 <body class="bg-gray-100 font-family-inter flex">
 
     <aside class="relative bg-[#152A38] h-screen w-64 hidden sm:block shadow-xl">
@@ -64,12 +64,12 @@
                 Daftar Berkas
             </a>
             <a href="krip.php" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-              <i class="fas fa-book-reader mr-3"></i>
-              KRIP
+                <i class="fas fa-book-reader mr-3"></i>
+                KRIP
             </a>
             <a href="saldo.php" class="flex items-center active-nav-link text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-              <i class="fas fa-money-bill mr-3"></i>
-              Cek Saldo
+                <i class="fas fa-money-bill mr-3"></i>
+                Cek Saldo
             </a>
         </nav>
         <a href="#" class="absolute w-full upgrade-btn bottom-0 active-nav-link text-white flex items-center justify-center py-4">
@@ -106,112 +106,36 @@
             <!-- Dropdown Nav -->
             <nav :class="isOpen ? 'flex': 'hidden'" class="flex flex-col pt-4">
                 <a href="index.php" class="flex items-center text-white py-2 pl-4 nav-item">
-                  <i class="fas fa-tachometer-alt mr-3"></i>
-                  Dashboard
+                    <i class="fas fa-tachometer-alt mr-3"></i>
+                    Dashboard
                 </a>
                 <a href="daftar.php" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                  <i class="fas fa-sticky-note mr-3"></i>
-                  Daftar Berkas
+                    <i class="fas fa-sticky-note mr-3"></i>
+                    Daftar Berkas
                 </a>
                 <a href="krip.php" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                  <i class="fas fa-book-reader mr-3"></i>
-                  KRIP
+                    <i class="fas fa-book-reader mr-3"></i>
+                    KRIP
                 </a>
                 <a href="saldo.php" class="flex items-center active-nav-link text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
                     <i class="fas fa-money-bill mr-3"></i>
                     Cek Saldo
                 </a>
                 <button class="w-full bg-white cta-btn font-semibold py-2 mt-3 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
-                  <i class="fas fa-arrow-alt-circle-left mr-3"></i>
-                  Log Out
+                    <i class="fas fa-arrow-alt-circle-left mr-3"></i>
+                    Log Out
                 </button>
             </nav>
         </header>
-    
-        <div class="w-full h-screen overflow-x-hidden border-t flex flex-col">
-            <main class="w-full flex-grow p-6">
-                <h1 class="w-full text-3xl text-black pb-6">Forms</h1>
 
-                <div class="flex flex-wrap">
-                    <div class="w-full lg:w-1/2 my-6 pr-0 lg:pr-2">
-                        <p class="text-xl pb-6 flex items-center">
-                            <i class="fas fa-list mr-3"></i> Contact Form
-                        </p>
-                        <div class="leading-loose">
-                            <form class="p-10 bg-white rounded shadow-xl">
-                                <div class="">
-                                    <label class="block text-sm text-gray-600" for="name">Name</label>
-                                    <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="name" name="name" type="text" required="" placeholder="Your Name" aria-label="Name">
-                                </div>
-                                <div class="mt-2">
-                                    <label class="block text-sm text-gray-600" for="email">Email</label>
-                                    <input class="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded" id="email" name="email" type="text" required="" placeholder="Your Email" aria-label="Email">
-                                </div>
-                                <div class="mt-2">
-                                    <label class=" block text-sm text-gray-600" for="message">Message</label>
-                                    <textarea class="w-full px-5 py-2 text-gray-700 bg-gray-200 rounded" id="message" name="message" rows="6" required="" placeholder="Your inquiry.." aria-label="Email"></textarea>
-                                </div>
-                                <div class="mt-6">
-                                    <button class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded" type="submit">Submit</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+        </main>
 
-                    <div class="w-full lg:w-1/2 mt-6 pl-0 lg:pl-2">
-                        <p class="text-xl pb-6 flex items-center">
-                            <i class="fas fa-list mr-3"></i> Checkout Form
-                        </p>
-                        <div class="leading-loose">
-                            <form class="p-10 bg-white rounded shadow-xl">
-                                <p class="text-lg text-gray-800 font-medium pb-4">Customer information</p>
-                                <div class="">
-                                    <label class="block text-sm text-gray-600" for="cus_name">Name</label>
-                                    <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="cus_name" name="cus_name" type="text" required="" placeholder="Your Name" aria-label="Name">
-                                </div>
-                                <div class="mt-2">
-                                    <label class="block text-sm text-gray-600" for="cus_email">Email</label>
-                                    <input class="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required="" placeholder="Your Email" aria-label="Email">
-                                </div>
-                                <div class="mt-2">
-                                    <label class=" block text-sm text-gray-600" for="cus_email">Address</label>
-                                    <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required="" placeholder="Street" aria-label="Email">
-                                </div>
-                                <div class="mt-2">
-                                    <label class="text-sm block text-gray-600" for="cus_email">City</label>
-                                    <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required="" placeholder="City" aria-label="Email">
-                                </div>
-                                <div class="inline-block mt-2 w-1/2 pr-1">
-                                    <label class="block text-sm text-gray-600" for="cus_email">Country</label>
-                                    <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required="" placeholder="Country" aria-label="Email">
-                                </div>
-                                <div class="inline-block mt-2 -mx-1 pl-1 w-1/2">
-                                    <label class="block text-sm text-gray-600" for="cus_email">Zip</label>
-                                    <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email"  name="cus_email" type="text" required="" placeholder="Zip" aria-label="Email">
-                                </div>
-                                <p class="text-lg text-gray-800 font-medium py-4">Payment information</p>
-                                <div class="">
-                                    <label class="block text-sm text-gray-600" for="cus_name">Card</label>
-                                    <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_name" name="cus_name" type="text" required="" placeholder="Card Number MM/YY CVC" aria-label="Name">
-                                </div>
-                                <div class="mt-6">
-                                    <button class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded" type="submit">$3.00</button>
-                                </div>
-                            </form>
-                        </div>
-                        <p class="pt-6 text-gray-600">
-                            Source: <a class="underline" href="https://tailwindcomponents.com/component/checkout-form">https://tailwindcomponents.com/component/checkout-form</a>
-                        </p>
-                    </div>
-                </div>
-            </main>
-    
-            <footer class="w-full bg-white text-center p-4">
-                Copyright to <a target="_blank" href="https://github.com/Queniex/Aplikasi-Pensiun" class="underline text-[#152A38] hover:text-blue-500">Kelompok 3</a><br>
-                All Right Reserved
-            </footer>
-        </div>
-        
+        <footer class="w-full bg-white text-center p-4">
+            Copyright to <a target="_blank" href="https://github.com/Queniex/Aplikasi-Pensiun" class="underline text-[#152A38] hover:text-blue-500">Kelompok 3</a><br>
+            All Right Reserved
+        </footer>
+    </div>
+
     </div>
 
     <!-- AlpineJS -->
@@ -219,4 +143,5 @@
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
 </body>
+
 </html>

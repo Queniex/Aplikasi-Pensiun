ee48124876f2489985d80d3fc857ec99939545df
adeliactra <adelia021203@gmail.com>
2023-01-09 12:46:31 +0700

    update

--- Public/Admin/kelolauser.php
+++ Public/Admin/kelolauser.php
@@ -259,7 +259,13 @@ if ( isset($_POST["delete"]) ){
                 <div class="p-6 text-center">
                     <input type="hidden" name="id" value="<?= $data["id_user"]; ?>">  
                     <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Anda yakin ingin menghapus akun user : <br> <?= $data["nama"]; ?>?</h3>
+                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Anda yakin ingin menghapus akun user : <br> 
+                    <?php if ($data["nama"] > 0) : ?>
+                        <?= $data["nama"]; ?>
+                    <?php else : ?>
+                        user :<?= $data["id_user"]; ?>?
+                    <?php endif ?>
+                    </h3>
                     <button data-modal-hide="delete-modal<?= $data["id_user"]; ?>" name="delete" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                         IYA
                     </button>
